2b2e472b0c86569a20f0f65445fa98c8b224bd0f
KarinaMonteiro <karina.kk93@gmail.com>
2022-10-09 16:12:31 -0300

    Criando navigationController para chamar uma tela com segue sendo referenciado pelo botão

---
+++ navegationComSeguePeloBotao/navegationComSeguePeloBotao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navegationComSeguePeloBotao
+//
+//  Created by Tiago & Karina on 09/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
